6ba41b8b8c4b37f72aeb2ad63d10b085269dc87e
toshihiko <greeeen8111@yahoo.co.jp>
2022-05-12 12:17:16 +0900

    first-commit

---
+++ Slime.java
@@ -0,0 +1,14 @@
+public class Slime extends Battle{
+
+    // スライムのコンストラクタ
+    /**
+     * スライムのコンストラクタ
+     * @param name 名前
+     * @param hp 体力
+     * @param mp マジックポイント
+     */
+    public Slime(String name, int hp, int mp) {
+    super(name, hp, mp);
+    }
+
+}
